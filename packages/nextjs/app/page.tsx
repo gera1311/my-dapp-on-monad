f91e4ec8ebbe633caa99fd19f734322d3cdcadd2
gera1311 <gera.python@yandex.ru>
2025-02-21 14:45:55 +0300

    Lesson 'Chapter 4 Wagmi'

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -2,13 +2,16 @@
 
 // import Link from "next/link";
 import type { NextPage } from "next";
+import { useAccount, useBalance } from "wagmi";
 
-// import { useAccount } from "wagmi";
 // import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 // import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
-  // const { address: connectedAddress } = useAccount();
+  const { address, chain, isConnected } = useAccount();
+  const result = useBalance({
+    address: address,
+  });
 
   return (
     <>
@@ -16,10 +19,20 @@ const Home: NextPage = () => {
         <div className="col-span-6">Walllet Connection Status</div>
         <div className="col-span-6">Network</div>
         <div className="col-span-6">
-          <input type="text" placeholder="Not Connected!" className="input !input-error rounded-md w-full" disabled />
+          <input
+            type="text"
+            placeholder={isConnected ? "Connected" : "Not Connected"}
+            className={`input ${isConnected ? "!input-success" : "!input-error"} rounded-md w-full`}
+            disabled
+          />
         </div>
         <div className="col-span-6">
-          <input type="text" placeholder="No Network" className="input !input-error rounded-md w-full" disabled />
+          <input
+            type="text"
+            placeholder={isConnected ? chain?.name || "Unknow" : "No Network"}
+            className={`input ${isConnected ? "!input-success" : "!input-error"} rounded-md w-full`}
+            disabled
+          />
         </div>
       </div>
       <div className="grid grid-cols-12 gap-x-4 mx-5 my-5">
@@ -27,13 +40,29 @@ const Home: NextPage = () => {
         <div className="col-span-4">Monad Name Service</div>
         <div className="col-span-4">Wallet Balance</div>
         <div className="col-span-4">
-          <input type="text" placeholder="0x" className="input !input-error rounded-md w-full" disabled />
+          <input
+            type="text"
+            placeholder={isConnected ? address : "Not Connected"}
+            className={`input ${isConnected ? "!input-success" : "!input-error"} rounded-md w-full`}
+            disabled
+          />
         </div>
         <div className="col-span-4">
           <input type="text" placeholder="xxxxx.nad" className="input !input-error rounded-md w-full" disabled />
         </div>
         <div className="col-span-4">
-          <input type="text" placeholder="0 MON" className="input !input-error rounded-md w-full" disabled />
+          <input
+            type="text"
+            placeholder={
+              result.isLoading
+                ? "Loading..."
+                : result.data
+                  ? `${result.data.formatted} ${result.data.symbol}`
+                  : "Not Connected"
+            }
+            className={`input ${isConnected ? "!input-success" : "!input-error"} rounded-md w-full`}
+            disabled
+          />
         </div>
       </div>
       <div className="grid grid-cols-8 gap-x-4 mx-5 my-5">
